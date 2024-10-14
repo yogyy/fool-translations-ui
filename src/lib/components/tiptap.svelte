149b9a9b980b0d032c37e5961fb6fb92036e702b
yogyy <yogimr261@gmail.com>
2024-10-14 22:15:40 +0700

    feat: add tiptap
    idk how to use quill, lmao

---
+++ src/lib/components/tiptap.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import { onMount, onDestroy } from 'svelte';
+  import { Editor } from '@tiptap/core';
+  import StarterKit from '@tiptap/starter-kit';
+  import Placeholder from '@tiptap/extension-placeholder';
+  import { cn } from '$lib/utils';
+
+  export let content: string;
+  export let placeholder = 'Write something …';
+  let className: string;
+  export { className as class };
+
+  let element: Element;
+  let editor: Editor;
+
+  onMount(() => {
+    editor = new Editor({
+      element: element,
+      extensions: [
+        StarterKit,
+        Placeholder.configure({
+          placeholder
+        })
+      ],
+      content,
+      onTransaction: () => {
+        // force re-render so `editor.isActive` works as expected
+        editor = editor;
+      },
+      onUpdate(props) {
+        content = props.editor.getText();
+      },
+
+      editorProps: {
+        attributes: {
+          class: cn(
+            'focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring',
+            className
+          )
+        }
+      }
+    });
+  });
+
+  onDestroy(() => {
+    if (editor) {
+      editor.destroy();
+    }
+  });
+</script>
+
+<div bind:this={element}>
+  <textarea class="hidden" {...$$restProps} bind:value={content} />
+</div>
