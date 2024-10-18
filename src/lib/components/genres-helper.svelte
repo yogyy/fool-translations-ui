98eeb2752001708db006d846032cf7c2652c8e1d
yogyy <yogimr261@gmail.com>
2024-10-18 19:49:58 +0700

    fix: remove missing function add handler

--- src/lib/components/genres-helper.svelte
+++ src/lib/components/genres-helper.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
-  import { addComa } from '$lib/utils';
   import { toast } from 'svelte-sonner';
 
   export let genres: string;
-</script>
 
-<button
-  class="text-sm italic text-accent"
-  type="button"
-  on:click={() => {
-    let msg = addComa(genres);
+  function genrehandler() {
+    let msg = genres.replace(/([a-z])([A-Z])/g, '$1, $2');
     if (genres.length === 0) {
       toast.info('genre is empty');
     } else if (genres.includes(',')) {
@@ -17,6 +12,9 @@
     } else {
       genres = msg;
     }
-  }}>
+  }
+</script>
+
+<button class="text-sm italic text-sky-400 dark:text-sky-800" type="button" on:click={genrehandler}>
   helper
 </button>
