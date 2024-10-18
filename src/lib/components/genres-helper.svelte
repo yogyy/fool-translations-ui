26cdf6d4e1fd7a30e86f47d7d1c60c17fd4e77ed
yogyy <yogimr261@gmail.com>
2024-10-18 20:23:20 +0700

    update toast position?

--- src/lib/components/genres-helper.svelte
+++ src/lib/components/genres-helper.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-  import { toast } from 'svelte-sonner';
+  import { toast, type ExternalToast } from 'svelte-sonner';
 
   export let genres: string;
 
+  const toastSetting: ExternalToast = { position: 'top-left' };
   function genrehandler() {
     let msg = genres.replace(/([a-z])([A-Z])/g, '$1, $2');
     if (genres.length === 0) {
-      toast.info('genre is empty');
+      toast.info('genre is empty', toastSetting);
     } else if (genres.includes(',')) {
-      toast.error('genre already separated');
+      toast.error('genre already separated', toastSetting);
     } else {
       genres = msg;
     }
