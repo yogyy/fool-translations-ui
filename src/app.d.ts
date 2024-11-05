15d5433e6d70b639f492851de1163ce8d47dfaf1
yogyy <yogimr261@gmail.com>
2024-11-05 22:16:34 +0700

    feat: add hooks for auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { Session, User } from '$lib/types';
+
 // for information about these interfaces
 declare global {
   namespace App {
     // interface Error {}
-    // interface Locals {}
+    interface Locals {
+      user: User | null;
+      session: Session | null;
+    }
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
---
+++ src/hooks.server.ts
@@ -0,0 +1,26 @@
+import {
+  deleteSessionTokenCookie,
+  setSessionTokenCookie,
+  validateSessionToken
+} from '$lib/session';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const token = event.cookies.get('session') ?? null;
+  if (token === null) {
+    event.locals.user = null;
+    event.locals.session = null;
+    return resolve(event);
+  }
+
+  const { session, user } = await validateSessionToken(event);
+  if (session !== null) {
+    setSessionTokenCookie(event, token, session.expiresAt);
+  } else {
+    deleteSessionTokenCookie(event);
+  }
+
+  event.locals.session = session;
+  event.locals.user = user;
+  return resolve(event);
+};
