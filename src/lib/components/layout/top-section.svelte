cb1275759e126a9769f5889727d824a22594eab1
yogyy <yogimr261@gmail.com>
2025-01-29 23:22:43 +0700

    chore: remove transition, remove settled variable

--- src/lib/components/layout/top-section.svelte
+++ src/lib/components/layout/top-section.svelte
@@ -10,29 +10,36 @@
 
   let api: CarouselAPI;
   let count = 0;
-  let current = 0;
+  let selected = 0;
 
   const plugin = Autoplay({ delay: 9500, stopOnInteraction: true });
 
   $: if (api) {
     count = api.scrollSnapList().length;
-    current = api.selectedScrollSnap() + 1;
 
     setTweenFactor(api);
     tweenOpacity(api);
 
-    api.on('select', () => {
-      current = api.selectedScrollSnap() + 1;
-    });
+    api.on('select', () => (selected = api.selectedScrollSnap()));
     api
       .on('reInit', setTweenFactor)
       .on('reInit', tweenOpacity)
       .on('scroll', tweenOpacity)
       .on('slideFocus', tweenOpacity);
   }
+
+  function navigateOrScroll(e: MouseEvent, i: number) {
+    if (i !== selected) {
+      e.preventDefault();
+      api.scrollTo(i);
+    }
+  }
 </script>
 
-<section aria-labelledby="top-novels" class="order-first bg-complimentary md:order-3 md:border-b">
+<section
+  aria-labelledby="top-novels"
+  class="order-first bg-complimentary md:order-3 md:border-b"
+  id="top-novels">
   <div class="md:p-3">
     <h2 class="sr-only">Top Novels</h2>
     <Carousel.Root
@@ -47,9 +54,10 @@
           <Carousel.Item
             class="h-auto min-w-0 shrink-0 grow-0 basis-[100%] py-0.5 pl-4 min-[450px]:basis-[75%] sm:basis-[50%] md:basis-[40%] lg:basis-1/3 xl:basis-[25%] 2xl:basis-[20.1%]">
             <a
+              on:click={(e) => navigateOrScroll(e, i)}
               data-sveltekit-preload-data="tap"
               href={`/novels/${top.id.slice(4)}`}
-              class="flex h-full flex-col -outline-offset-0 transition duration-200 sm:rounded-xl">
+              class={cn('flex h-full flex-col -outline-offset-0 sm:rounded-xl')}>
               <div
                 class="relative flex-grow !border-0 p-0 before:absolute before:inset-0 before:bg-gradient-to-t before:from-zinc-950/80 before:from-0% before:via-zinc-950/55 before:via-30% before:to-transparent before:to-60% before:sm:rounded-xl">
                 <img
@@ -61,8 +69,8 @@
                   loading="eager" />
                 <div
                   class={cn(
-                    'absolute bottom-0 left-0 right-0 mb-4 flex flex-col justify-center px-2 py-1 transition-opacity delay-100 duration-500 md:px-4 md:py-2',
-                    current === i + 1 ? 'block opacity-100' : 'hidden opacity-0'
+                    'absolute bottom-0 left-0 right-0 mb-4 flex flex-col justify-center px-2 py-1 md:px-4 md:py-2',
+                    selected === i ? 'block' : 'hidden'
                   )}>
                   <div
                     class="mx-auto rounded-md px-4 py-2 sm:px-2 md:px-2 lg:px-4 xl:px-2 2xl:px-4">
@@ -78,8 +86,8 @@
                 </div>
                 <p
                   class={cn(
-                    'absolute bottom-2 right-4 px-2 py-0 text-xs leading-none transition-opacity duration-300',
-                    current === i + 1 ? 'block opacity-100' : 'hidden opacity-0'
+                    'absolute bottom-2 right-4 px-2 py-0 text-xs leading-none',
+                    selected === i ? 'block' : 'hidden'
                   )}>
                   <span class="text-white/90">{i + 1}</span>
                   <span class="text-white/90">/</span>
