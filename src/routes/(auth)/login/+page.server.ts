402dcd52bbc15aec7ee548ab47b124bf888b04eb
yogyy <yogimr261@gmail.com>
2025-04-12 23:18:40 +0700

    refactor

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -5,39 +5,30 @@ import { zod } from 'sveltekit-superforms/adapters';
 import { loginSchema, type AuthResponse } from '../schema';
 import { BE_URL } from '$env/static/private';
 import { parseString } from 'set-cookie-parser';
-import { deleteSessionTokenCookie, setSessionTokenCookie } from '$lib/session';
+import { setSessionTokenCookie } from '$lib/session';
 
 export const load: PageServerLoad = async (event) => {
-  const referer = event.cookies.get('referer');
   if (event.locals.user !== null) {
     return redirect(302, '/');
   }
 
-  return {
-    form: await superValidate(zod(loginSchema)),
-    referer
-  };
+  return { form: await superValidate(zod(loginSchema)) };
 };
 
 export const actions: Actions = {
   default: async (event) => {
     const form = await superValidate(event, zod(loginSchema));
-    if (!form.valid) {
-      return fail(400, {
-        form
-      });
-    }
+    if (!form.valid) return fail(400, { form });
 
     const res = await fetch(`${BE_URL}/auth/signin`, {
       method: 'POST',
       body: JSON.stringify(form.data),
-      headers: { 'Content-type': 'application/json' },
-      credentials: 'include'
+      headers: { 'Content-type': 'application/json' }
     });
     const signin: AuthResponse = await res.json();
     if (signin.success) {
       const cookie = parseString(res.headers.get('set-cookie')!);
-      setSessionTokenCookie(event, signin.token, cookie.expires as Date);
+      setSessionTokenCookie(event, cookie.value, cookie.expires as Date);
     }
 
     return { form, signin };
