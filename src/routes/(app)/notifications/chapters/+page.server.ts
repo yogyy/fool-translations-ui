395368fb3e2e001255130a055077b3b1dd05cb47
yogyy <yogimr261@gmail.com>
2025-04-13 22:25:42 +0700

    chore: remove console.log

--- src/routes/(app)/notifications/chapters/+page.server.ts
+++ src/routes/(app)/notifications/chapters/+page.server.ts
@@ -1,12 +1,10 @@
-import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { BE_URL } from '$env/static/private';
 import type { ApiResponse, Notification } from '$lib/types';
 
-export const load: PageServerLoad = async (event) => {
-  if (event.locals.user) {
-    console.log('ngefetch nih bos');
-    const res = await event.fetch(`${BE_URL}/notifications`);
+export const load: PageServerLoad = async ({ fetch, locals }) => {
+  if (locals.user) {
+    const res = await fetch(`${BE_URL}/notifications`);
     const data: ApiResponse<Notification[]> = await res.json();
 
     return { chapters: data.data };
