23ba9ca3df5f32779666a038e800a4104be342a6
yogyy <yogimr261@gmail.com>
2025-04-16 19:58:21 +0700

    fix color

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,9 +5,9 @@
 <svelte:head>
   <title>{$page.status} | {$page.error?.message}</title>
 </svelte:head>
-<div class="flex min-h-dvh flex-col items-center justify-center gap-3 bg-black">
-  <h1 class="text-3xl font-medium uppercase text-foreground">
+<div class="flex min-h-dvh flex-col items-center justify-center gap-3 bg-black font-mono">
+  <h1 class="text-3xl font-medium uppercase text-cyan-50">
     <span class="font-semibold">{$page.status}</span> | {$page.error?.message}
   </h1>
-  <a href="/" class="rounded-sm border px-2 py-1.5 hover:text-cyan-300">RETURN</a>
+  <a href="/" class="rounded-sm border px-2 py-1.5 text-cyan-700 hover:text-cyan-300">RETURN</a>
 </div>
