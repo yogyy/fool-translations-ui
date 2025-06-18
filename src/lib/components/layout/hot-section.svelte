19f315c54c8f828408c06869d186ad8e4bba4abd
yogyy <yogimr261@gmail.com>
2025-06-18 20:15:09 +0700

    chore: feat add lazy load for hot section

--- src/lib/components/layout/hot-section.svelte
+++ src/lib/components/layout/hot-section.svelte
@@ -4,30 +4,44 @@
   import type { CarouselAPI } from '$lib/components/ui/carousel/context';
   import Autoplay from 'embla-carousel-autoplay';
 
-  export let data: HotNovel[];
-  let api: CarouselAPI;
-  let count = 0;
-  let current = 0;
+  import { setupLazyLoadImage } from '../ui/carousel/lazy-load';
+
+  interface Props {
+    data: HotNovel[];
+  }
+  let { data }: Props = $props();
+
+  let api = $state<CarouselAPI>();
+  let current = $state(0);
 
   const plugin = Autoplay({ delay: 9500, stopOnInteraction: true });
 
-  $: if (api) {
-    count = api.scrollSnapList().length;
-    current = api.selectedScrollSnap() + 1;
-    api.on('select', () => {
+  $effect(() => {
+    if (api) {
       current = api.selectedScrollSnap() + 1;
-    });
-  }
+      api.on('select', () => {
+        current = api!.selectedScrollSnap() + 1;
+      });
+
+      const loadImagesInView = setupLazyLoadImage(api);
+
+      api
+        .on('init', loadImagesInView)
+        .on('reInit', loadImagesInView)
+        .on('slidesInView', loadImagesInView);
+    }
+  });
 </script>
 
-<section class="order-3 border-b bg-complimentary md:order-first md:border-none md:bg-transparent">
-  <div class="relative mx-auto flex md:max-w-[90rem]">
+<section
+  class="bg-complimentary order-3 mb-10 border-b md:order-first md:border-none md:bg-transparent">
+  <div class="relative mx-auto flex aspect-[2.6] md:max-w-[90rem]">
     <div
       class="absolute flex h-auto w-full justify-between self-center text-cyan-700 dark:text-cyan-400">
       <button
         type="button"
-        class="z-20 rounded-md transition-colors hover:bg-foreground/10"
-        on:click={() => api.scrollPrev()}>
+        class="hover:bg-foreground/10 z-20 rounded-md transition-colors"
+        onclick={() => api?.scrollPrev()}>
         <span class="sr-only">Prev</span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -45,8 +59,8 @@
 
       <button
         type="button"
-        class="z-20 rounded-md transition-colors hover:bg-foreground/10"
-        on:click={() => api.scrollNext()}>
+        class="hover:bg-foreground/10 z-20 rounded-md transition-colors"
+        onclick={() => api?.scrollNext()}>
         <span class="sr-only">Next</span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -64,19 +78,25 @@
     </div>
     <div class="my-5">
       <h2 class="sr-only">Featured Banner</h2>
-      <Carousel.Root class="relative" bind:api opts={{ loop: true }} plugins={[plugin]}>
+      <Carousel.Root
+        class="embla__viewport relative overflow-hidden"
+        setApi={(emblaApi) => (api = emblaApi)}
+        opts={{ loop: true }}
+        plugins={[plugin]}>
         <Carousel.Content>
-          {#each data as hot, idx}
+          {#each data as hot}
             <Carousel.Item class="min-w-0 shrink-0 grow-0 basis-full pl-4">
               <div
-                class="rounded-xl border border-none bg-transparent text-card-foreground shadow-none">
-                <a href={`/novels/${hot.novelId.slice(4)}`}>
-                  <div class="relative flex flex-grow justify-center !border-0 p-0">
+                class="text-card-foreground rounded-xl border border-none bg-transparent shadow-none">
+                <a data-sveltekit-preload-data="tap" href={`/novels/${hot.novelId.slice(4)}`}>
+                  <div
+                    class="relative flex h-full w-full grow items-center justify-center !border-0 p-0">
+                    <span class="embla__lazy-load__spinner"></span>
                     <img
-                      src={hot.image}
+                      src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D"
+                      data-src={hot.image}
                       alt={`top novels ${hot.novelId.slice(4)}`}
-                      class="featured-image"
-                      loading={idx > 1 ? 'lazy' : 'eager'} />
+                      class="featured-image embla__slide__img embla__lazy-load__img" />
                   </div>
                 </a>
               </div>
@@ -88,7 +108,7 @@
   </div>
 </section>
 
-<style scoped>
+<style>
   .featured-image {
     object-fit: cover;
     max-width: 1400px;
--- src/lib/components/layout/top-section.svelte
+++ src/lib/components/layout/top-section.svelte
@@ -4,59 +4,60 @@
   import type { CarouselAPI } from '$lib/components/ui/carousel/context';
   import Autoplay from 'embla-carousel-autoplay';
   import { cn } from '$lib/utils';
-  import { setTweenFactor, tweenOpacity } from '../ui/carousel/tween-opacity';
+  import { setTweenFactor, tweenOpacity } from '$lib/components/ui/carousel/tween-opacity';
 
-  export let data: Topnovel[];
+  let { data }: { data: Topnovel[] } = $props();
 
-  let api: CarouselAPI;
-  let count = 0;
-  let selected = 0;
-
+  let api = $state<CarouselAPI>();
+  const count = $derived(api ? api.scrollSnapList().length : 0);
+  let selected = $state(0);
   const plugin = Autoplay({ delay: 9500, stopOnInteraction: true });
 
-  $: if (api) {
-    count = api.scrollSnapList().length;
+  $effect(() => {
+    if (api) {
+      selected = api.selectedScrollSnap();
 
-    setTweenFactor(api);
-    tweenOpacity(api);
+      setTweenFactor(api);
+      tweenOpacity(api);
 
-    api.on('select', () => (selected = api.selectedScrollSnap()));
-    api
-      .on('reInit', setTweenFactor)
-      .on('reInit', tweenOpacity)
-      .on('scroll', tweenOpacity)
-      .on('slideFocus', tweenOpacity);
-  }
+      api.on('select', () => (selected = api!.selectedScrollSnap()));
+      api
+        .on('reInit', setTweenFactor)
+        .on('reInit', tweenOpacity)
+        .on('scroll', tweenOpacity)
+        .on('slideFocus', tweenOpacity);
+    }
+  });
 
   function navigateOrScroll(e: MouseEvent, i: number) {
     if (i !== selected) {
       e.preventDefault();
-      api.scrollTo(i);
+      api?.scrollTo(i);
     }
   }
 </script>
 
-<section class="order-first bg-complimentary md:order-3 md:border-b">
+<section class="bg-complimentary order-first md:order-3 md:border-b">
   <div class="md:p-3">
     <h2 class="sr-only">Top Novels</h2>
     <Carousel.Root
       class="relative"
       opts={{ align: 'center', loop: true }}
-      bind:api
+      setApi={(emblaApi) => (api = emblaApi)}
       plugins={[plugin]}
-      on:mousenter={plugin.stop}
-      on:mouseleave={() => plugin.play()}>
+      onmouseenter={plugin.stop}
+      onmouseleave={() => plugin.play()}>
       <Carousel.Content>
         {#each data as top, i}
           <Carousel.Item
             class="h-auto min-w-0 shrink-0 grow-0 basis-[100%] py-0.5 pl-4 min-[450px]:basis-[75%] sm:basis-[50%] md:basis-[40%] lg:basis-1/3 xl:basis-[25%] 2xl:basis-[20.1%]">
             <a
-              on:click={(e) => navigateOrScroll(e, i)}
+              onclick={(e) => navigateOrScroll(e, i)}
               data-sveltekit-preload-data="tap"
               href={`/novels/${top.id.slice(4)}`}
               class={cn('flex h-full flex-col -outline-offset-0 sm:rounded-xl')}>
               <div
-                class="relative flex-grow !border-0 p-0 before:absolute before:inset-0 before:bg-gradient-to-t before:from-zinc-950/80 before:from-0% before:via-zinc-950/55 before:via-30% before:to-transparent before:to-60% before:sm:rounded-xl">
+                class="relative grow !border-0 p-0 before:absolute before:inset-0 before:bg-gradient-to-t before:from-zinc-950/80 before:from-0% before:via-zinc-950/55 before:via-30% before:to-transparent before:to-60% before:sm:rounded-xl">
                 <img
                   src={top.cover}
                   alt={top.title}
@@ -65,7 +66,7 @@
                   loading={i <= 2 || i >= 8 ? 'eager' : 'lazy'} />
                 <div
                   class={cn(
-                    'absolute bottom-0 left-0 right-0 mb-4 flex flex-col justify-center px-2 py-1 md:px-4 md:py-2',
+                    'absolute right-0 bottom-0 left-0 mb-4 flex flex-col justify-center px-2 py-1 md:px-4 md:py-2',
                     selected === i ? 'block' : 'hidden'
                   )}>
                   <div
@@ -82,7 +83,7 @@
                 </div>
                 <p
                   class={cn(
-                    'absolute bottom-2 right-4 px-2 py-0 text-xs leading-none',
+                    'absolute right-4 bottom-2 px-2 py-0 text-xs leading-none',
                     selected === i ? 'block' : 'hidden'
                   )}>
                   <span class="text-white/90">{i + 1}</span>
