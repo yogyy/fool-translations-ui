fe8f41ec3ef56b91cca081c4fd42b0c634f571e2
yogyy <yogimr261@gmail.com>
2025-06-18 18:20:40 +0700

    fix sitemap #6 ?

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -2,37 +2,35 @@ export async function GET() {
   return new Response(
     `
 		<?xml version="1.0" encoding="UTF-8" ?>
-		<urlset
-			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-			xmlns:xhtml="https://www.w3.org/1999/xhtml"
-			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-		>
+		<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
             <url>
             <loc>https://fool-translations.pages.dev</loc>
-            <changefreq>daily</changefreq>
+            <lastmod>2025-06-18T10:31:22.001Z</lastmod>
+            <changefreq>monthly</changefreq>
             <priority>0.7</priority>
             </url>
             <url>
             <loc>https://fool-translations.pages.dev/novels</loc>
-            <changefreq>daily</changefreq>
+            <lastmod>2025-06-18T10:31:22.001Z</lastmod>
+            <changefreq>monthly</changefreq>
             <priority>0.7</priority>
             </url>
             <url>
             <loc>https://fool-translations.pages.dev/novels/vwy5c7jn6wdhadev</loc>
-            <changefreq>daily</changefreq>
+            <lastmod>2025-06-18T10:31:22.001Z</lastmod>
+            <changefreq>monthly</changefreq>
             <priority>0.7</priority>
             </url>
             <url>
             <loc>https://fool-translations.pages.dev/login</loc>
-            <changefreq>daily</changefreq>
+            <lastmod>2025-06-18T10:31:22.001Z</lastmod>
+            <changefreq>monthly</changefreq>
             <priority>0.7</priority>
             </url>
             <url>
             <loc>https://fool-translations.pages.dev/register</loc>
-            <changefreq>daily</changefreq>
+            <lastmod>2025-06-18T10:31:22.001Z</lastmod>
+            <changefreq>monthly</changefreq>
             <priority>0.7</priority>
             </url>
 		</urlset>`.trim(),
