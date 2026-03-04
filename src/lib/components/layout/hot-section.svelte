a520e27029abe05fd2773f036c767d4d420db99e
yogyy <yogimr261@gmail.com>
2026-03-05 00:50:47 +0700

    feat: new ui

--- src/lib/components/layout/hot-section.svelte
+++ src/lib/components/layout/hot-section.svelte
@@ -3,8 +3,13 @@
   import type { HotNovel } from '$lib/types';
   import type { CarouselAPI } from '$lib/components/ui/carousel/context';
   import Autoplay from 'embla-carousel-autoplay';
-
-  import { setupLazyLoadImage } from '../ui/carousel/lazy-load';
+  import { cn } from '$lib/utils';
+  import DotFilled from '../icons/dot-filled.svelte';
+  import PlayCircle from '../icons/play-circle.svelte';
+  import StopCircle from '../icons/stop-circle.svelte';
+  import Progress from '../ui/progress/progress.svelte';
+  import { Tween } from 'svelte/motion';
+  import { cubicInOut } from 'svelte/easing';
 
   interface Props {
     data: HotNovel[];
@@ -13,109 +18,164 @@
 
   let api = $state<CarouselAPI>();
   let current = $state(0);
+  let startAutoPlay = $state(false);
+  let delay = 2500;
 
-  const plugin = Autoplay({ delay: 9500, stopOnInteraction: true });
+  const plugin = Autoplay({
+    playOnInit: false,
+    delay,
+    stopOnInteraction: true,
+    active: true
+  });
+
+  const tween = new Tween(0, { duration: 2200, easing: cubicInOut });
+  let timer: ReturnType<typeof setTimeout> | null = null;
+
+  function startProgress() {
+    if (!api) return;
+
+    const autoplay = api.plugins().autoplay;
+    delay = autoplay?.options.delay ?? 2500;
+    startAutoPlay = true;
+    tween.set(0, { duration: 0 });
+
+    // kasih sedikit delay supaya DOM update dulu
+    requestAnimationFrame(() => {
+      tween.set(100, { duration: delay, easing: (t) => t });
+    });
+  }
+
+  function stopProgress() {
+    tween.set(0, { duration: 0 });
+  }
 
   $effect(() => {
+    console.log(startAutoPlay);
     if (api) {
-      current = api.selectedScrollSnap() + 1;
+      current = api.selectedScrollSnap();
+      startAutoPlay = api?.plugins().autoplay.isPlaying() || false;
+
       api.on('select', () => {
-        current = api!.selectedScrollSnap() + 1;
+        current = api!.selectedScrollSnap();
+        // Always reset tween and timer, regardless of startAutoPlay,
+        // to ensure progress resets when changing slide selection.
+        stopProgress();
+        if (startAutoPlay) {
+          startProgress();
+        }
       });
 
-      const loadImagesInView = setupLazyLoadImage(api);
+      api.on('autoplay:play', () => {
+        startProgress();
+      });
 
-      api
-        .on('init', loadImagesInView)
-        .on('reInit', loadImagesInView)
-        .on('slidesInView', loadImagesInView);
+      api.on('autoplay:stop', () => {
+        stopProgress();
+      });
     }
+
+    return () => {
+      if (timer) clearTimeout(timer);
+      timer = null;
+    };
   });
+
+  function navigateOrScroll(e: MouseEvent, i: number) {
+    if (i !== current) {
+      e.preventDefault();
+      api?.scrollTo(i);
+    }
+  }
 </script>
 
-<section
-  class="bg-complimentary order-3 mb-10 border-b md:order-first md:border-none md:bg-transparent">
-  <div class="relative mx-auto flex aspect-[2.6] md:max-w-[90rem]">
-    <div
-      class="absolute flex h-auto w-full justify-between self-center text-cyan-700 dark:text-cyan-400">
-      <button
-        type="button"
-        class="hover:bg-foreground/10 z-20 rounded-md transition-colors"
-        onclick={() => api?.scrollPrev()}>
-        <span class="sr-only">Prev</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round">
-          <path d="m15 18-6-6 6-6" />
-        </svg>
-      </button>
-
-      <button
-        type="button"
-        class="hover:bg-foreground/10 z-20 rounded-md transition-colors"
-        onclick={() => api?.scrollNext()}>
-        <span class="sr-only">Next</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round">
-          <path d="m9 18 6-6-6-6" />
-        </svg>
-      </button>
-    </div>
-    <div class="my-5">
+<section class="bg-complimentary order-3 mb-10 md:order-first md:bg-transparent">
+  <div class="relative mx-auto flex">
+    <div class="mx-auto flex px-5">
       <h2 class="sr-only">Featured Banner</h2>
       <Carousel.Root
-        class="embla__viewport relative overflow-hidden"
         setApi={(emblaApi) => (api = emblaApi)}
-        opts={{ loop: true }}
-        plugins={[plugin]}>
-        <Carousel.Content>
-          {#each data as hot}
-            <Carousel.Item class="min-w-0 shrink-0 grow-0 basis-full pl-4">
-              <div
-                class="text-card-foreground rounded-xl border border-none bg-transparent shadow-none">
-                <a data-sveltekit-preload-data="tap" href={`/novels/${hot.novelId.slice(4)}`}>
-                  <div
-                    class="relative flex h-full w-full grow items-center justify-center !border-0 p-0">
-                    <span class="embla__lazy-load__spinner"></span>
-                    <img
-                      src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D"
-                      data-src={hot.image}
-                      alt={`top novels ${hot.novelId.slice(4)}`}
-                      class="featured-image embla__slide__img embla__lazy-load__img" />
-                  </div>
-                </a>
-              </div>
+        opts={{ loop: true, align: 'center' }}
+        plugins={[plugin]}
+        class="">
+        <Carousel.Content class="mx-auto flex max-w-4xl">
+          {#each data as hot, i}
+            <Carousel.Item
+              class={cn(
+                'aspect-square max-w-[22rem] overflow-hidden rounded-md pl-0.5 transition-[opacity] sm:basis-1/3',
+                current === i ? 'opacity-100' : 'opacity-40'
+              )}>
+              <a
+                class={cn(
+                  'relative flex w-full',
+                  current === i ? 'cursor-pointer' : 'cursor-default'
+                )}
+                onclick={(e) => navigateOrScroll(e, i)}
+                href={`/novels/${hot.novelId.slice(4)}`}>
+                <img src={hot.image} alt="" class="w-full object-cover object-top" />
+              </a>
             </Carousel.Item>
           {/each}
         </Carousel.Content>
+        <Carousel.Previous
+          class="bg-sky-300/5 text-sky-300"
+          onclick={() => {
+            api?.scrollPrev();
+
+            if (startAutoPlay) {
+              api?.plugins().autoplay.reset();
+            }
+          }} />
+        <Carousel.Next
+          class="bg-sky-300/5 text-sky-300"
+          onclick={() => {
+            api?.scrollNext();
+
+            if (startAutoPlay) {
+              api?.plugins().autoplay.reset();
+            }
+          }} />
       </Carousel.Root>
     </div>
   </div>
+  <div
+    class="text-muted-foreground mx-auto hidden items-center justify-center py-2 text-center text-sm lg:flex">
+    {#each data as _, i}
+      {#if current === i}
+        <Progress class="h-1 w-10" value={tween.current} />
+      {:else}
+        <button
+          onclick={() => {
+            api?.scrollTo(i);
+            if (startAutoPlay) {
+              api?.plugins().autoplay.reset();
+            }
+          }}>
+          <span class="sr-only">scroll to {i}</span>
+          <DotFilled class="size-4" />
+        </button>
+      {/if}
+    {/each}
+    <!-- auto play button -->
+    {#if startAutoPlay}
+      <button
+        class="ml-1 flex items-center justify-center rounded-full"
+        onclick={() => {
+          startAutoPlay = false;
+          api?.plugins().autoplay.stop();
+        }}>
+        <span class="sr-only">stop autoplay</span>
+        <StopCircle class="text-destructive" />
+      </button>
+    {:else}
+      <button
+        class="ml-1 flex items-center justify-center rounded-full"
+        onclick={() => {
+          api?.plugins().autoplay.play();
+          startAutoPlay = true;
+        }}>
+        <span class="sr-only">start autoplay</span>
+        <PlayCircle class="text-sky-300" />
+      </button>
+    {/if}
+  </div>
 </section>
-
-<style>
-  .featured-image {
-    object-fit: cover;
-    max-width: 1400px;
-    max-height: 535px;
-    aspect-ratio: 2.616822429906542;
-    width: 100%;
-    border-radius: 0.75rem;
-    object-position: top;
-  }
-</style>
